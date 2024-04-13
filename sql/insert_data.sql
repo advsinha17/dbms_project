8e2464e19e723302f8313df5bd10c70ee6d712db
Archith <vk.archith@gmail.com>
2024-04-13 15:06:49 +0530

    data

---
+++ sql/insert_data.sql
@@ -0,0 +1,20 @@
+CALL create_new_restaurant(1000,'Yumpies','Variety of Indian and Chinese dishes, along with Maggi noodles served','Open','1234567890');
+CALL create_new_restaurant(1001,'Thickshake Company','Milkshakes,thickshakes and icecream avalailable','Closed','2345678901');
+CALL create_new_restaurant(1002,'Chipotle','Delicious south indian breakfast served','Open','3456789012');
+CALL create_new_restaurant(1003,'Wichplease','Variety of sandwiches and maggi served','Closed','4567890123');
+CALL create_new_restaurant(1004,'Chai Coffee Company','chinese cuisine along with indian sweets and snacks served','Permanently Closed','5678901234');
+CALL create_new_restaurant(1005,'Maggi Hotspot','100 different varities of maggi served along with some beverages','Open','6789012345');
+CALL create_new_restaurant(1006,'SFC','continental cuisine along with packaged ice cream and deserts served here','Under Renovation','7890123456');
+CALL create_new_restaurant(1007,'Chaat Place','Indian chaat items available','Open','8901234567');
+
+CALL add_user(10000001,'Advik','Sinha','f20222004@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000002,'Archith','Vinod Kumar','f20220229@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000003,'Rohan','Mathew','f20220211@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000004,'Arnav','Gupta','f20220067@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000005,'Aarav','Haran','f20220880@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000006,'Kevin','George Mathew','f20220238@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000007,'Aditya','Pentyala','f20220522@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000008,'Abishek','Muthukumar','f20220770@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000009,'Saharsh','Misra','f20220074@hyderabad.bits-pilani.ac.in');
+CALL add_user(10000010,'Abinav','Venkatagiri','f20222002@hyderabad.bits-pilani.ac.in');
+
